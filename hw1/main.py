537f5be0a2504c506a8da12dad623b2ac92d3f26
vsplekhanov <sclerothorax@gmail.com>
2022-01-23 15:46:29 +0300

    done fibonacci

--- hw1/main.py
+++ hw1/main.py
@@ -1,1 +1,11 @@
-print("hw")
+def fibonacci(n: int) -> int:
+    curr, prev = 1, 1
+    for i in range(n - 2):
+        tmp = curr + prev
+        prev = curr
+        curr = tmp
+    return curr
+
+
+if __name__ == '__main__':
+    print(fibonacci(int(input())))
