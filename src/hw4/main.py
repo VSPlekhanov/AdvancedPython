03bd43d27361725ee1ef7d68e9704e98f0de5774
vsplekhanov <sclerothorax@gmail.com>
2022-03-08 23:48:58 +0300

    done easy

--- src/hw4/main.py
+++ src/hw4/main.py
@@ -1,6 +1,7 @@
 from hw1.src.plekhanov_hw1 import fibonacci
 from time import time
 import threading
+import multiprocessing
 
 
 def measure(f):
@@ -19,4 +20,19 @@ def use_threads(count, n):
         t.join()
 
 
-print(measure(lambda: use_threads(10, 500000)))
+def use_processes(count, n):
+    processes = []
+    for _ in range(count):
+        p = multiprocessing.Process(target=fibonacci.fibonacci, args=(n,))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
+
+
+if __name__ == '__main__':
+    with open("artifacts/result.txt", "w") as out:
+        proc_time = str(measure(lambda: use_processes(10, 500000)))
+        threads_time = str(measure(lambda: use_threads(10, 500000)))
+        out.write(f"Processes time: {proc_time}s\n")
+        out.write(f"Threads time: {threads_time}s\n")
