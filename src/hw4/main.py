250c7c6ca90c26b341e21d91ecbe70d501c74182
vsplekhanov <sclerothorax@gmail.com>
2022-03-08 23:25:22 +0300

    add threads

--- src/hw4/main.py
+++ src/hw4/main.py
@@ -1,6 +1,22 @@
 from hw1.src.plekhanov_hw1 import fibonacci
 from time import time
+import threading
 
-t = time()
-[fibonacci.fibonacci(250000) for _ in range(10)]
-print(time() - t)
+
+def measure(f):
+    now = time()
+    f()
+    return time() - now
+
+
+def use_threads(count, n):
+    threads = []
+    for _ in range(count):
+        t = threading.Thread(target=fibonacci.fibonacci, args=(n,))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+
+print(measure(lambda: use_threads(10, 500000)))
